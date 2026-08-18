1acf7eb4d4dc20138b696ebdbc9b5935f427ab61
ORVEAX <orveax@gmail.com>
2026-08-19 02:17:14 +0300

    refactor: simplify Vue integration after Pinia removal

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,5 @@ export default defineConfig({
   site: 'https://targetft.com',
   output: 'static',
   build: { format: 'file' },
-  integrations: [vue({ appEntrypoint: '/src/pages/_app' })],
-  vite: {
-    define: {
-      __VUE_OPTIONS_API__: JSON.stringify(true),
-      __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
-      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
-    },
-    ssr: {
-      noExternal: ['pinia'],
-    },
-  },
+  integrations: [vue()],
 });
